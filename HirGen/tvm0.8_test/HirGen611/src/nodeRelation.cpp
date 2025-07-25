ddbba8eaecf4c3956f0d1bf505e52112fb1778fd
WGQZYJZ <1074781857@qq.com>
2025-07-25 16:43:14 +0800

    delete

--- HirGen/tvm0.8_test/HirGen611/src/nodeRelation.cpp
+++
@@ -1,5 +0,0 @@
-#include <nodeRelation.hpp>
-
-std::map<NODE_PTR, NODE_PTRs> nodeRelation::parents = {};
-std::map<NODE_PTR, NODE_PTRs> nodeRelation::children = {};
-std::set<std::tuple<int, int>> nodeRelation::parent_child = {};
